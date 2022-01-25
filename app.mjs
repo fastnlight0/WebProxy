579f117832b30b97b7691292f3784d9bb9b38243
fastnlight0 <admin@cryptonote.live>
2022-01-24 18:28:41 -0600

    Initial commit

---
+++ app.mjs
@@ -0,0 +1,44 @@
+import express from 'express'
+import scrape from 'website-scraper'
+import fs from 'fs'
+
+var app = express()
+
+app.use(express.static(import.meta.url.split('file://')[1] + '/../public'))
+app.get('/', function (req, res) {
+    res.render('index')
+})
+
+app.get('/get/*', function(req, res){
+    if (req.url == "/get/"){
+        res.redirect('/get')
+        
+    } else{
+        fs.rmSync("public/del", { recursive: true, force: true })
+        console.log("Executed rm")
+        console.log("Starting download on: " + req.url.split("/get/")[1])
+        let options = {
+            urls: [req.url.split("/get/")[1]],
+            directory: ("public/del"),
+        };
+        
+        scrape(options).then((result) => {
+            console.log("Downloaded: " + req.url.split("/get/")[1])
+            res.redirect("/del")
+        }).catch((err) => {
+            console.log("Error downloading: " + req.url.split("/get/")[1] + ", " + err)
+            res.send('<script>alert("Error downloading: ' + req.url.split("/get/")[1] + ', ' + err + '"); window.location.href = "/"; </script>')
+        });
+    }
+})
+
+app.get('/rm', function(req, res) {
+    fs.rmSync("public/del", { recursive: true, force: true })
+    console.log("Executed rm")
+    res.redirect("/")
+})
+
+
+
+
+app.listen(8888)
