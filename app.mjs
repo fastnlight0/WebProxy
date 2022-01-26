5bab61818cf69103990eb5962e342bf7214236e8
BuildTools <unconfigured@null.spigotmc.org>
2022-01-25 18:10:17 -0600

    a

--- app.mjs
+++ app.mjs
@@ -3,7 +3,7 @@ import scrape from 'website-scraper'
 import fs from 'fs'
 
 var app = express()
-var url = new URL()
+var url = ""
 
 app.use(express.static(import.meta.url.split('file://')[1] + '/../public'))
 app.get('/', function (req, res) {
@@ -13,13 +13,12 @@ app.get('/', function (req, res) {
 app.get('/get/*', function (req, res) {
     if (req.url == "/get/") {
         res.redirect('/get')
-
     } else {
         fs.rmSync("public/del", { recursive: true, force: true })
         console.log("Executed rm")
-        url = new URL()
+        url = ""
         console.log("Starting download on: " + req.url.split("/get/")[1])
-        url = new URL(req.url.split("/get/")[1])
+        url = req.url.split("/get/")[1]
         let options = {
             urls: [req.url.split("/get/")[1]],
             directory: ("public/del"),
@@ -37,16 +36,17 @@ app.get('/get/*', function (req, res) {
 
 app.get('/rm', function (req, res) {
     fs.rmSync("public/del", { recursive: true, force: true })
-    url = new URL()
+    url = ""
     console.log("Executed rm")
     res.redirect("/")
 })
 
 app.get('/red/*', function (req, res) {
-    if (req.url == "/red/") {
+    if (req.url == "/red/" || url == "") {
         res.redirect('/red')
     } else {
-        res.redirect('/get/' + url.protocol + "//" + url.hostname+"/"+req.url.split("/red/")[1])
+        var urlt = new URL(url)
+        res.redirect('/get/' + urlt.protocol + "//" + urlt.hostname+"/"+req.url.split("/red/")[1])
     }
 })
 
@@ -54,5 +54,5 @@ app.get('/red/*', function (req, res) {
 app.listen(process.env.PORT)
 fs.rmSync("public/del", { recursive: true, force: true })
 console.log("Executed rm")
-url = new URL()
+url = ""
 console.log("WebDel online")
